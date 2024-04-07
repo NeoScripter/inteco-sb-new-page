eca11e34b7375b4e4868f829545337bb719aa4d6
Ilya <sange0337@gmail.com>
2024-04-07 17:24:03 +0800

    Finished the reviews section

--- hmp.js
+++ hmp.js
@@ -1,3 +1,4 @@
+// Navigation large icon section
 
 const emergingField = document.querySelector('.nav-emerging-par');
 
@@ -10,3 +11,31 @@ document.querySelectorAll('.nav-large-icon').forEach(socialLink => {
     });
 });
 
+
+// Reviews sections gradually appearing and disappearing
+document.querySelectorAll('.expand_button').forEach(button => {
+    button.addEventListener('click', function() {
+        const review = this.previousElementSibling;
+        const expandedText = review.querySelector('.expanded');
+        const ellipsis = review.querySelector('.ellipsis');
+
+        // Toggle visibility
+        if (expandedText.style.display === "none" || !expandedText.style.display) {
+            expandedText.style.display = 'inline';
+            setTimeout(() => { 
+                expandedText.style.opacity = '1'; 
+              }, 150);
+            ellipsis.style.display = 'none';
+            this.textContent = 'Скрыть';
+            button.style.setProperty('--expand-button-after-width', '18%');
+        } else {
+            expandedText.style.opacity = '0'; 
+            setTimeout(() => { 
+                expandedText.style.display = 'none';
+                ellipsis.style.display = 'inline';
+                this.textContent = 'Читать полностью';
+                button.style.removeProperty('--expand-button-after-width');
+              }, 150);
+        }
+    });
+});
